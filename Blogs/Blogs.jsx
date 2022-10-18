02032adf44d5c3b69ed45806d6f5a1a13228778e
Deja Mann <110996266+DejaMann@users.noreply.github.com>
2022-10-17 21:21:32 -0400

    auth/auth

--- Blogs/Blogs.jsx
+++ Blogs/Blogs.jsx
@@ -1,12 +1,56 @@
+// === mapping out what to show on ShowBlog.jsx
 const React = require('react')
+const Navbar = require('../ReusableReactComponents/Navbar')
 
 class Blogs extends React.Component {
     render(){
-        console.log(this.props)
-        return <div>
-            <h1>Blogs</h1> 
-        </div>
+        const {blogs, loggedInUser} = this.props; // destructuring
+            return ( // add styles to div
+            <div>
+                <Navbar />
+                <head>
+                    <link rel="stylesheet" href="/CSS/app.css" />
+                </head>
+                <h1>Blogs</h1>
+
+                <section style = {styles.container}> {blogs.map((blog) => (
+                    <div style = {styles.wrapper} className='card'> 
+                        <a href={`/blog/${blog._id}`}> 
+                        {'  '}
+                     <h2>{blog.title}</h2>
+                       </a>  
+                       <div>
+                        <p>{blog.body}</p>
+                       </div> 
+                        <h5>Written by: {blog.author}</h5>
+                        {blog.author === loggedInUser ? (<div>
+                            <a href = {`/blog/${blog._id}/edit`}>Edit</a>
+                        </div>
+                        ) : null}
+                    </div>
+                    ))}
+                </section>
+            </div>                
+        );       
     }
 }
 
-module.exports = Blogs;
+const styles = {
+    container: {
+        display: 'flex',
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'center'
+    },
+    wrapper: {
+        padding: '20px',
+        margin: '10px',
+        width: '300px',
+        border: 'solid',
+        borderWidth: '2px',
+        borderColor: 'black'
+    },
+}
+
+
+module.exports = Blogs
